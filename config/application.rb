a1bf710c8973ff90695f1575a1dfdfa0833c46f5
Ashna Shah-Grover <as19@williams.edu>
2019-10-22 17:54:58 -0400

    incorporating IBM Watson

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+require "ibm_watson"
+
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
---
+++ config/initializers/ibm_watson_setup.rb
@@ -0,0 +1,36 @@
+require "ibm_watson/authenticators"
+require "ibm_watson/tone_analyzer_v3"
+include IBMWatson
+
+NLU_APIKEY = Rails.application.credentials.NLU_IAM_KEY
+# VERSION = ENV['ibm_watson_version']
+NLU_URL = Rails.application.credentials.NLU_URL
+
+PI_APIKEY= Rails.application.credentials.PI_IAM_KEY
+# VERSION = ENV['ibm_watson_version']
+PI_URL = Rails.application.credentials.PI_URL
+
+
+nlu_authenticator = Authenticators::IamAuthenticator.new(
+    apikey: NLU_APIKEY
+)
+
+pi_authenticator = Authenticators::IamAuthenticator.new(
+    apikey: PI_APIKEY
+)
+
+NLU = NaturalLanguageUnderstandingV1.new(
+    version: "2019-07-12",
+    authenticator: nlu_authenticator
+)
+
+PI = PersonalityInsightsV3.new(
+  version: "2017-10-13",
+  authenticator: pi_authenticator
+)
+
+NLU.service_url = NLU_URL
+NLU
+
+PI.service_url = PI_URL
+PI
